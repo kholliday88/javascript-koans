4f0b677137ab664ed4feef1df9fe8f7ae08256cb
kholliday88 <kholliday88@gmail.com>
2016-03-21 14:48:44 -0400

    Koans

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -1,73 +1,73 @@
-describe("About Functions", function () {
+describe("About Functions", function() {
 
-    it("should declare functions", function () {
+  it("should declare functions", function() {
 
-        function add(a, b) {
-            return a + b;
-        }
+    function add(a, b) {
+      return a + b;
+    }
 
-        expect(add(1, 2)).toBe(FILL_ME_IN);
-    });
+    expect(add(1, 2)).toBe(3);
+  });
 
-    it("should know internal variables override outer variables", function () {
-        var message = "Outer";
+  it("should know internal variables override outer variables", function() {
+    var message = "Outer";
 
-        function getMessage() {
-            return message;
-        }
+    function getMessage() {
+      return message;
+    }
 
-        function overrideMessage() {
-            var message = "Inner";
-            return message;
-        }
+    function overrideMessage() {
+      var message = "Inner";
+      return message;
+    }
 
-        expect(getMessage()).toBe(FILL_ME_IN);
-        expect(overrideMessage()).toBe(FILL_ME_IN);
-        expect(message).toBe(FILL_ME_IN);
-    });
+    expect(getMessage()).toBe('Outer');
+    expect(overrideMessage()).toBe('Inner');
+    expect(message).toBe('Outer');
+  });
 
-    it("should allow extra function arguments", function () {
+  it("should allow extra function arguments", function() {
 
-        function returnFirstArg(firstArg) {
-            return firstArg;
-        }
+    function returnFirstArg(firstArg) {
+      return firstArg;
+    }
 
-        expect(returnFirstArg("first", "second", "third")).toBe(FILL_ME_IN);
+    expect(returnFirstArg("first", "second", "third")).toBe("first", "second", "third");
 
-        function returnSecondArg(firstArg, secondArg) {
-            return secondArg;
-        }
+    function returnSecondArg(firstArg, secondArg) {
+      return secondArg;
+    }
 
-        expect(returnSecondArg("only give first arg")).toBe(FILL_ME_IN);
+    expect(returnSecondArg("only give first arg")).toBe(undefined);
 
-        function returnAllArgs() {
-            var argsArray = [];
-            for (var i = 0; i < arguments.length; i += 1) {
-                argsArray.push(arguments[i]);
-            }
-            return argsArray.join(",");
-        }
+    function returnAllArgs() {
+      var argsArray = [];
+      for (var i = 0; i < arguments.length; i += 1) {
+        argsArray.push(arguments[i]);
+      }
+      return argsArray.join(",");
+    }
 
-        expect(returnAllArgs("first", "second", "third")).toBe(FILL_ME_IN);
-    });
+    expect(returnAllArgs("first", "second", "third")).toBe("first,second,third");
+  });
 
-    it("should pass functions as values", function () {
+  it("should pass functions as values", function() {
 
-        var appendRules = function (name) {
-            return name + " rules!";
-        };
+    var appendRules = function(name) {
+      return name + " rules!";
+    };
 
-        var appendDoubleRules = function (name) {
-            return name + " totally rules!";
-        };
+    var appendDoubleRules = function(name) {
+      return name + " totally rules!";
+    };
 
-        var praiseSinger = {
-            givePraise: appendRules
-        };
-        expect(praiseSinger.givePraise("John")).toBe(FILL_ME_IN);
+    var praiseSinger = {
+      givePraise: appendRules
+    };
+    expect(praiseSinger.givePraise("John")).toBe('John rules!');
 
-        praiseSinger.givePraise = appendDoubleRules;
-        expect(praiseSinger.givePraise("Mary")).toBe(FILL_ME_IN);
+    praiseSinger.givePraise = appendDoubleRules;
+    expect(praiseSinger.givePraise("Mary")).toBe('Mary totally rules!');
 
-    });
+  });
 });
--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -1,99 +1,102 @@
 var _; //globals
 
-/* 
+/*
 This section uses a functional extension known as Underscore.js - http://underscorejs.org/
-    
+
     "Underscore is a utility-belt library for JavaScript that provides a lot of the functional programming support that you would expect in Prototype.js (or Ruby), but without extending any of the built-in JavaScript objects."
-    
+
 Underscore is very similar to Lodash; it basically provides a bunch of useful functions to use on arrays.
 Note that the underscore library is loaded in automatically via a HTML tag and can be used via the '_' (underscore) object.
 
 The function _([1, 2, 3]) is provided by underscore and converts the JS array into an Underscore array, which
 is required for many / most Underscore features.
  */
-describe("About Higher Order Functions", function () {
+describe("About Higher Order Functions", function() {
 
-    it("should use filter to return array items that meet a criteria", function () {
-        var numbers = [1, 2, 3];
-        // % = modulus operator
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder_()
-        var odd = numbers.filter(function (x) {
-            return x % 2 !== 0;
-        });
-
-        expect(odd).toEqual(FILL_ME_IN);
-        expect(odd.length).toBe(FILL_ME_IN);
-        expect(numbers.length).toBe(FILL_ME_IN);
+  it("should use filter to return array items that meet a criteria", function() {
+    var numbers = [1, 2, 3];
+    // % = modulus operator
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder_()
+    var odd = numbers.filter(function(x) {
+      return x % 2 !== 0;
     });
 
-    it("should use 'map' to transform each element", function () {
-        var numbers = [1, 2, 3];
-        var numbersPlus1 = numbers.map(function (x) {
-            return x + 1
-        });
+    expect(odd).toEqual([1, 3]);
+    expect(odd.length).toBe(2);
+    expect(numbers.length).toBe(3);
+  });
 
-        expect(numbersPlus1).toEqual(FILL_ME_IN);
-        expect(numbers).toEqual(FILL_ME_IN);
+  it("should use 'map' to transform each element", function() {
+    var numbers = [1, 2, 3];
+    var numbersPlus1 = numbers.map(function(x) {
+      return x + 1
     });
 
-    it("should use 'reduce' to update the same result on each iteration", function () {
-        var numbers = [1, 2, 3];
-        var reduction = numbers.reduce(
-            function ( /* result from last call */ memo, /* current */ x) {
-                return memo + x
-            }, /* initial */ 0);
+    expect(numbersPlus1).toEqual([2, 3, 4]);
+    expect(numbers).toEqual([1, 2, 3]);
+  });
 
-        expect(reduction).toBe(FILL_ME_IN);
-        expect(numbers).toEqual(FILL_ME_IN);
-    });
+  it("should use 'reduce' to update the same result on each iteration", function() {
+    var numbers = [1, 2, 3];
+    var reduction = numbers.reduce(
+      function( /* result from last call */ memo, /* current */ x) {
+        return memo + x
+      }, /* initial */ 0);
 
-    it("should use 'forEach' for simple iteration", function () {
-        var numbers = [1, 2, 3];
-        var msg = "";
-        var isEven = function (item) {
-            msg += (item % 2) === 0;
-        };
+    expect(reduction).toBe(6);
+    expect(numbers).toEqual([1, 2, 3]);
+  });
 
-        // New function!
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-        numbers.forEach(isEven);
+  it("should use 'forEach' for simple iteration", function() {
+    var numbers = [1, 2, 3];
+    var msg = "";
+    var isEven = function(item) {
+      msg += (item % 2) === 0;
+    };
 
-        expect(msg).toEqual(FILL_ME_IN);
-        expect(numbers).toEqual(FILL_ME_IN);
-    });
+    // New function!
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+    numbers.forEach(isEven);
 
-    it("should use 'all' to test whether all items pass condition", function () {
-        var onlyEven = [2, 4, 6];
-        var mixedBag = [2, 4, 5, 6];
+    expect(msg).toEqual("falsetruefalse");
+    expect(numbers).toEqual([1, 2, 3]);
+  });
 
-        var isEven = function (x) {
-            return x % 2 === 0;
-        };
+  it("should use 'all' to test whether all items pass condition", function() {
+    var onlyEven = [2, 4, 6];
+    var mixedBag = [2, 4, 5, 6];
 
-        // http://underscorejs.org/#every
-        expect(_(onlyEven).every(isEven)).toBe(FILL_ME_IN);
-        expect(_(mixedBag).every(isEven)).toBe(FILL_ME_IN);
-    });
+    var isEven = function(x) {
+      return x % 2 === 0;
+    };
 
-    it("should use 'any' to test if any items passes condition", function () {
-        var onlyEven = [2, 4, 6];
-        var mixedBag = [2, 4, 5, 6];
+    // http://underscorejs.org/#every
+    expect(_(onlyEven).every(isEven)).toBe(true);
+    expect(_(mixedBag).every(isEven)).toBe(false);
+  });
 
-        var isEven = function (x) {
-            return x % 2 === 0
-        };
+  it("should use 'any' to test if any items passes condition", function() {
+    var onlyEven = [2, 4, 6];
+    var mixedBag = [2, 4, 5, 6];
 
-        expect(_(onlyEven).any(isEven)).toBe(FILL_ME_IN);
-        expect(_(mixedBag).any(isEven)).toBe(FILL_ME_IN);
-    });
+    var isEven = function(x) {
+      return x % 2 === 0
+    };
 
-    it("should use range to generate an array", function () {
-        expect(_.range(3)).toEqual(FILL_ME_IN);
-        expect(_.range(1, 4)).toEqual(FILL_ME_IN);
-        expect(_.range(0, -4, -1)).toEqual(FILL_ME_IN);
-    });
+    expect(_(onlyEven).any(isEven)).toBe(true);
+    expect(_(mixedBag).any(isEven)).toBe(true);
+  });
 
-    it("should use flatten to make nested arrays easy to work with", function () {
-        expect(_([[1, 2], [3, 4]]).flatten()).toEqual(FILL_ME_IN);
-    });
+  it("should use range to generate an array", function() {
+    expect(_.range(3)).toEqual([0, 1, 2]);
+    expect(_.range(1, 4)).toEqual([1, 2, 3]);
+    expect(_.range(0, -4, -1)).toEqual([0, -1, -2, -3]);
+  });
+
+  it("should use flatten to make nested arrays easy to work with", function() {
+    expect(_([
+      [1, 2],
+      [3, 4]
+    ]).flatten()).toEqual([1, 2, 3, 4]);
+  });
 });
